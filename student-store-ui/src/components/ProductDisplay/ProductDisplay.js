13c77b9d7d34b04211f89bc962653e6974a6a911
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-17 11:42:43 -0700

    retrieves products from api

--- student-store-ui/src/components/ProductDisplay/ProductDisplay.js
+++ student-store-ui/src/components/ProductDisplay/ProductDisplay.js
@@ -1,13 +1,40 @@
 import axios from "axios"
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import "./ProductDisplay.css"
 
 
 function ProductDisplay () {
 
+    /* STATES */
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState(null);
+    const [products, setProducts] = useState([])
+
+
     useEffect (() => {
         
+        const fetchProducts = async () => {
+            setIsFetching(true);
+            
+            try {
+                let res = await axios.get("http://localhost:3001/store/products");
+                let products = res?.data?.products;
+                console.log(res.data.products)
+                
+                if (products) //if products exists
+                    setProducts(res);
+
+            } catch (err) {
+                setError(err);
+            }
+
+
+            setIsFetching(false);
+        }
+
+        fetchProducts();
+
     }, [])
 
     return (
