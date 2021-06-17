81508dd896cf622a8964bc24eb07433faceaa345
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-16 19:04:51 -0700

    adds handlers for 404 errors and generic errors

--- student-store-api/server.js
+++ student-store-api/server.js
@@ -1,9 +1,10 @@
 
-const express = require("express")
-const morgan = require("morgan")
+const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 
-const storeRouter = require("./routes/store")
+const storeRouter = require("./routes/store");
+const { NotFoundError } = require("./utils/errors");
 
 const app = express();
 
@@ -11,9 +12,24 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
 
+app.use("/store", storeRouter);
 
 
 
+/* Handle all 404 errors that weren't matched by a route */
+app.get((req, res, next) => {
+    return next(new NotFoundError());
+})
+
+/* Generic error handler - anything that is unhandled will be handled here */
+app.get((error, req, res, next) => {
+    const status = error.status || 500;
+    const message = error.message;
+
+    res.status(status).json({
+        error: { message, status }
+    })
+})
 
 const port = 3001;
 
