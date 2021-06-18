9262d184cf47c9fb5e256bb32100f6750d6db5e7
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-18 15:01:07 -0700

    adds opening sidebar

--- student-store-ui/src/components/CheckoutBar/CheckoutBar.js
+++ student-store-ui/src/components/CheckoutBar/CheckoutBar.js
@@ -1,11 +1,23 @@
 import './CheckoutBar.css';
 
+import { useState } from "react"
 
 function CheckoutBar () {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleArrowOnClick = () => {
+        if (isOpen)
+            setIsOpen(false);
+        else if (!isOpen)
+            setIsOpen(true);
+    } 
+
+
     return (
-        <section className="CheckoutBar closed">
+        <section className={`CheckoutBar ${isOpen ? "open" : "closed"}`}>
             <div className="content-wrapper">
-                <span className="arrow-button closed">
+                <span className={`arrow-button ${isOpen ? "open" : "closed"}`} onClick={handleArrowOnClick}>
                     <i className="material-icons md-48">arrow_forward</i>
                 </span>
 
