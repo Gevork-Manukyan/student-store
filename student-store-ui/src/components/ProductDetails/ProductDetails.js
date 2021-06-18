2f6b919287d7958dc85fff8a47955fa8b2d22178
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-17 19:08:15 -0700

    lays out skeleton for productDetails

--- student-store-ui/src/components/ProductDetails/ProductDetails.js
+++ student-store-ui/src/components/ProductDetails/ProductDetails.js
@@ -34,11 +34,39 @@ function ProductDetails (props) {
 
     }, [productID]);
 
-    console.log(product)
-
+    
     return (
         <div className="ProductDetails">
-            
+            <div className="flex-container">
+                <div className="image-area">
+                    <img />
+                </div>
+                
+                <div className="information-area">
+                    <span className="product-title"></span>
+                    
+                    <div className="product-description">
+
+                    </div>
+
+                    <div className="checkout-area">
+                        <span className="price"></span>
+
+                        <div className="cart-buttons">
+                            <button className="minus-button"></button>
+
+                            <div className="cart-information">
+
+                                <span className="cart-count"></span>
+                                <button className="add-to-cart-button"></button>
+                                
+                            </div>
+                            
+                            <button className="plus-button"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
